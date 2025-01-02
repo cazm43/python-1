1d621c1bfd08dc210b3f3e187cf61c1e4267f981
brain2-prog <cazm43@hotmail.com>
2025-01-02 01:24:34 -0400

    lion

--- Python project.py
+++ Python project.py
@@ -1,11 +1,5 @@
 print("Hello World")
-country = input("What is your country?")
-if country == "China":
-    print("You can continue")
-else:
-    print("Sorry, you are not authorized")
-for i in range(10):
-    print(i)
+
 
 
 
