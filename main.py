1508f047d272646505b0307bd6ceee78af3bd49b
brain2-prog <cazm43@hotmail.com>
2025-01-02 01:26:34 -0400

    lion

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 print("Hello World")
-from datetime import datetime
-current_date = datetime.now()
-print(current_date)
 
-fruits = ['orange', 'pear', 'orange', 'kiwi,','pear']
-fruits.count ('orange')
